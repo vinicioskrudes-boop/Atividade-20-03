47b636a19def81337b0d8cb61de00fb7fd8e2930
vinicioskrudes-boop <vinicioskrudes@gmail.com>
2026-03-20 16:49:30 -0300

    Add files via upload

---
+++ LP/script.js
@@ -0,0 +1,9 @@
+function enviar (){
+    const nome = document.getElementById("nome").value
+    const assunto = document.getElementById("assunto").value
+    const mensagem = `Gostaria de entrar em contato! \n\nNome: ${nome} \nAssunto: ${assunto}`
+
+    const msg = encodeURIComponent(mensagem)
+    window.open(`https://wa.me/55***********?text = ${msg}`)
+
+}
